fcc4ef1e03d1f27687b34926678fffbf126dbf4e
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-10 16:24:23 -0500

    Nuevos estilos

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'appFooter',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
