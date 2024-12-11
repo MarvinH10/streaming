b31f07efc096f8678eb3cb410446aed576e4f93b
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-11 18:10:19 -0500

    about us

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HeroComponent,
+    AboutComponent,
   ],
   exports: [
     HeroComponent,
+    AboutComponent,
   ],
 })
 export class HomeModule {}
