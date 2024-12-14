e2aa5fb19d05a3096a39bd1b92f76ba7fad553fb
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-12-14 14:06:54 -0500

    Añadiendo peliculas

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
+import { ContenidoComponent } from './contenido/contenido.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HeroComponent,
     AboutComponent,
+    ContenidoComponent,
   ],
   exports: [
     HeroComponent,
     AboutComponent,
+    ContenidoComponent,
   ],
 })
-export class HomeModule {}
+export class HomeModule { }
